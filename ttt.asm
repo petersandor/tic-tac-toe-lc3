56dab797f530cbb14dab9b8480c242880b154c41
Peter Šándor <peter.sandor2@ibm.com>
2023-03-04 02:14:40 +0100

    add user input, pseudo-RNG

--- ttt.asm
+++ ttt.asm
@@ -19,6 +19,9 @@ GAME_LOOP
       ADD R0,R0,#0
       BRz GAME_OVER
 
+      LEA R0, PROMPT_MOVE_MESSAGE
+      JSR PROMPT
+
       JSR MAKE_MOVE ; (R0 > 0 = spot marked)
 
       LD R0,MOVES_LEFT
@@ -29,12 +32,40 @@ GAME_LOOP
 GAME_WIN
       LD R0,NEW_LINE
       OUT
-      OUT
       LEA R0, WIN_MESSAGE
       PUTS
 
 GAME_OVER
       HALT
+
+; data
+      STACK             .FILL x4000
+      BOARD             .FILL x0
+                        .FILL x0
+                        .FILL x0
+                        .FILL x0
+                        .FILL x0
+                        .FILL x0
+                        .FILL x0
+                        .FILL x0
+                        .FILL x0
+      MOVES_LEFT        .FILL #9
+      WINNER            .FILL x0
+
+      TEXT_BOARD_LABELS_TBL_PTR .FILL x3070 ; TEXT_BOARD_LABELS_TBL
+
+      WELCOME_MESSAGE   .STRINGZ "Welcome to LC-3 TTT minigame\n"
+      WIN_MESSAGE       .STRINGZ "You won!"  ; TODO: add symbol ref
+      DRAW_MESSAGE      .STRINGZ "It's a draw."
+      PROMPT_MOVE_MESSAGE .STRINGZ	"Choose position (1-9): "
+
+      TEXT_BOARD_LABELS_TBL .FILL x3073	; BOARD_LABEL_0
+                            .FILL x3075 ; BOARD_LABEL_1
+                            .FILL x3077 ; BOARD_LABEL_2
+
+      BOARD_LABEL_0     .STRINGZ	" "
+      BOARD_LABEL_1     .STRINGZ	"X"
+      BOARD_LABEL_2     .STRINGZ	"O"
 
 CHECK_WINNING_COMBINATION
       STR R7, R6, #-1     ; save registers
@@ -293,19 +324,6 @@ DRAW_EXIT
       HOR_DIV           .STRINGZ "\n-----------\n"
       VERT_DIV          .STRINGZ "|"
 
-; data
-      STACK             .FILL x4000
-      BOARD             .FILL x0
-                        .FILL x0
-                        .FILL x0
-                        .FILL x0
-                        .FILL x0
-                        .FILL x0
-                        .FILL x0
-                        .FILL x0
-                        .FILL x0
-      MOVES_LEFT        .FILL #9
-      WINNER            .FILL x0
 
       WIN_COMBINATIONS  .FILL #0
                         .FILL #1
@@ -341,18 +359,218 @@ DRAW_EXIT
                         .FILL #10
                         .FILL #10
 
-      TEXT_BOARD_LABELS_TBL_PTR .FILL x3123 ; TEXT_BOARD_LABELS_TBL
+;
+; Since LC-3 has no source of entropy for random number generation, the only
+; source of entropy can be extracted via user input. The below routine implements
+; a pseudo random number generator which is using the player's moves as a seed.
+;
 
-      WELCOME_MESSAGE   .STRINGZ "Welcome to LC-3 TTT minigame\n"
-      WIN_MESSAGE       .STRINGZ "You won!"  ; TODO: add symbol ref
-      DRAW_MESSAGE      .STRINGZ "It's a draw."
+;
+; From Ryan Pendleton's 2048
+;
 
-      TEXT_BOARD_LABELS_TBL .FILL x3126	; BOARD_LABEL_0
-                            .FILL x3128 ; BOARD_LABEL_1
-                            .FILL x312A ; BOARD_LABEL_2
+;--------------------------------------------------------------------------
+; RAND_MOD
+; Generates random number between 0 and r0 - 1 inclusively
+; Returns r0 = random number
+;--------------------------------------------------------------------------
 
-      BOARD_LABEL_0     .STRINGZ	" "
-      BOARD_LABEL_1     .STRINGZ	"X"
-      BOARD_LABEL_2     .STRINGZ	"O"
+RAND_MOD
+      STR   R0, R6, #-1
+      STR   R1, R6, #-2
+      STR   R2, R6, #-3
+      STR   R7, R6, #-4
+      ADD   R6, R6, #-4
+
+      LD    R0, RAND_SEED
+      LD    R1, RAND_Q
+      JSR   MOD_DIV           ; R0 = x % q
+
+      LD    R1, RAND_A
+      JSR   MULT              ; R0 = (x % q) * a
+      ST    R0, RAND_SEED
+
+      LDR   R1, R6, #3        ; get original R0
+      JSR   MOD_DIV
+
+      LDR   R7, R6, #0
+      LDR   R2, R6, #1
+      LDR   R1, R6, #2
+      ADD   R6, R6, #4
+      RET
+
+; data
+      RAND_INIT   .FILL x0000
+      RAND_SEED   .FILL xC20D
+      RAND_A      .FILL x0007
+      RAND_M      .FILL x7FFF ; 2^15 - 1
+      RAND_Q      .FILL x1249 ; M/A
+
+;--------------------------------------------------------------------------
+; GETC_SEED
+; Seeds random number generator while getting a character from the keyboard
+; Returns r0 = character
+;--------------------------------------------------------------------------
+
+GETC_SEED
+      STR   R1, R6, #-1       ; save R1
+      ADD   R6, R6, #-1
+
+      AND   R1, R1, #0
+GETC_SEED_LOOP                ; R1++ until character pressed
+      ADD   R1, R1, #1
+      LDI   R0, OS_KBSR
+      BRzp  GETC_SEED_LOOP
+
+      LD    R0, SEED_MASK
+      AND   R1, R1, R0
+
+      LDI   R0, OS_KBDR       ; get character
+      ST    R1, RAND_SEED     ; save R1 to seed
+      ST    R1, RAND_INIT     ; save initial for debugging
+
+      LDR   R1, R6, #0        ; restore R1
+      ADD   R6, R6, #1
+      RET
+
+; data
+      OS_KBSR     .FILL xFE00
+      OS_KBDR     .FILL xFE02
+      SEED_MASK   .FILL x7FFF
+
+;--------------------------------------------------------------------------
+; PROMPT
+; Prompts the user to choose position (0-9)
+; Returns r0 = 0-8
+;--------------------------------------------------------------------------
+
+PROMPT
+      STR   R0, R6, #-1       ; save registers
+      STR   R1, R6, #-2
+      STR   R7, R6, #-3
+      ADD   R6, R6, #-3
+
+PROMPT_LOOP                   ; prompt until valid value is provided
+      LDR   R0, R6, #2
+      PUTS
+
+      JSR   GETC_SEED
+      OUT
+
+      ADD   R1, R0, #0
+      LD    R0, NEW_LINE
+      OUT
+
+      ADD   R0, R1, #0
+      BRnz  PROMPT_INVALID            ; TODO check free spots
+      LD    R0, PROMPT_RESPONSE_max
+      ADD   R0, R0, R1
+      BRp   PROMPT_INVALID
+      ADD   R0, R0, #8
+      ADD   R2, R5, #0
+      ADD   R2, R2, R0
+      LDR   R2, R2, #0
+      BRp   PROMPT_INVALID
+      BR    PROMPT_EXIT
+
+PROMPT_INVALID
+      ADD   R0, R1, #0
+      OUT
+      LEA   R0, PROMPT_INVALID_MESSAGE
+      PUTS
+      BRnzp PROMPT_LOOP
+
+PROMPT_EXIT
+      LDR   R7, R6, #0        ; restore registers
+      LDR   R1, R6, #1
+      ADD   R6, R6, #3
+      AND   R2, R2, #0
+      RET
+
+; data
+      PROMPT_INVALID_MESSAGE  .STRINGZ    " is not a valid input.\n\n"
+      PROMPT_RESPONSE_max     .FILL #-57
+
+
+;--------------------------------------------------------------------------
+; MOD_DIV
+; Performs r0 % r1 and r0/r1.
+; Returns r0 = remainder, r1 = quotient
+;--------------------------------------------------------------------------
+
+MOD_DIV
+      STR   R1, R6, #-1       ; save registers
+      STR   R2, R6, #-2
+      STR   R3, R6, #-3
+      ADD   R6, R6, #-3
+
+      NOT   R2, R1
+      ADD   R2, R2, #1
+      BRz   MOD_DIV_EX        ; halt if dividing by zero
+
+      AND   R1, R1, #0        ; clear R1 (quotient)
+
+MOD_DIV_LOOP
+      ADD   R1, R1, #1
+      ADD   R0, R0, R2        ; R0 -= R1
+      BRp MOD_DIV_LOOP        ; R0 - R1 > 0, so keep looping
+      BRz MOD_DIV_END         ; R0 = 0, so we finished exactly
+
+                              ; R0 < 0, so we subtracted an extra one
+      LDR   R2, R6, #2        ; add it back in
+      ADD   R1, R1, #-1
+      ADD   R0, R0, R2
+
+MOD_DIV_END
+      LDR   R3, R6, #0
+      LDR   R2, R6, #1
+      ADD   R6, R6, #3
+      RET
+
+MOD_DIV_EX
+      HALT
+
+;--------------------------------------------------------------------------
+; MULT
+; Performs multiplication using bit shifting
+; Returns r0 = r0 * r1
+;--------------------------------------------------------------------------
+
+MULT
+      ADD   R0, R0, #0
+      BRz   MULT_ZERO   ; return 0 if R0 = 0
+      ADD   R1, R1, #0
+      BRz   MULT_ZERO   ; return 0 if R1 = 0
+
+      STR   R1, R6, #-1 ; save registers
+      STR   R2, R6, #-2 ; save registers
+      STR   R3, R6, #-3
+      STR   R4, R6, #-4
+      ADD   R6, R6, #-4
+
+      AND   R2, R2, #0  ; clear R2 (product)
+      ADD   R3, R2, #1  ; set R3 = 1 (bit tester)
+
+MULT_LOOP               ; for each bit in R0
+      AND   R4, R0, R3        ; R4 = bit test(R0, R3)
+      BRnz  #1                ; only execute next line if bit is set
+      ADD   R2, R2, R1              ; product = product + R1
+      ADD   R1, R1, R1        ; R1 << 1
+      ADD   R3, R3, R3        ; R3 << 1
+      BRp   MULT_LOOP
+
+      ADD   R0, R2, #0  ; move product to R0
+
+MULT_END
+      LDR   R4, R6, #0  ; restore registers
+      LDR   R3, R6, #1
+      LDR   R2, R6, #2
+      LDR   R1, R6, #3
+      ADD   R6, R6, #4
+      RET
+
+MULT_ZERO
+      AND   R0, R0, #0
+      RET
 
 .END
